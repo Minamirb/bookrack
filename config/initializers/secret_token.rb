5128abd542f89d488b57bd2692a6e918b1ebdb00
滝口 敦弘 <atakigu@MBA.local>
2010-11-27 17:00:16 +0900

    Inistialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookrack::Application.config.secret_token = '89dba606afa35b1181b32244644dcbf73c37c194ed137580c232f1ab9831ec16f49f7bee1091e9c63eaf6d393d24f98cd0bb80031c049c9d3d6261754fe92c49'
